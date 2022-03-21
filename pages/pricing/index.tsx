7deccf4eba91587b37bbffe4a3077e5487d068a0
Ivan Gutierrez <ivangutierz20@gmail.com>
2022-03-21 13:03:53 -0400

    Pre-deployement vercel

---
+++ pages/pricing/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import { MainLayout } from "../../components/layouts/MainLayout"
+
+
+export  const PricingPage = () => {
+    return (
+        <div>
+            <MainLayout>
+                <h1>Pricing Page</h1>
+                <h1 className={'title'}>
+                Ir a<Link href="/"  >Home</Link>
+                </h1>
+
+                <p className={'description'}>
+                Get started by editing{' '}
+                <code className={'code'}>pages/pricing/index.jsx</code>
+                </p>
+            </MainLayout>
+        </div>
+    )
+}
+
+export default PricingPage;
